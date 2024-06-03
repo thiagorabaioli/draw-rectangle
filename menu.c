f6b93ae4c68dfb85ec2555344e6f4a79d291c9a7
thiagorabaioli <tfrabaioli@outlook.com>
2024-06-03 11:20:31 +0100

    moveright and moveleft OK

--- menu.c
+++ menu.c
@@ -5,11 +5,15 @@
 #include "plano.h"
 
 void exibir_menu() {
-    printf("Comandos disponiveis:\n");
-    printf("  create x,y+l,h    - Cria um retangulo em (x,y) com largura l e altura h\n");
-    printf("  moveleft x,y+p    - Move o retangulo que contem o ponto (x,y) para a esquerda p posicoes\n");
-    printf("  moveright x,y+p   - Move o retangulo que contem o ponto (x,y) para a direita p posicoes\n");
+    printf("Comandos disponíveis:\n");
+    printf("  create x,y+l,h    - Cria um retângulo em (x,y) com largura l e altura h\n");
+    printf("  moveleft x,y+p    - Move o retângulo que contém o ponto (x,y) para a esquerda p posições\n");
+    printf("  moveright x,y+p   - Move o retângulo que contém o ponto (x,y) para a direita p posições\n");
     printf("  exit              - Encerra o programa\n");
+}
+
+void exibir_posicao_retangulo(const Retangulo* ret) {
+    printf("Posição atual do retângulo: (%d, %d)\n", ret->x, ret->y);
 }
 
 void processar_comando(Plano* plano, const char* comando) {
@@ -17,11 +21,13 @@ void processar_comando(Plano* plano, const char* comando) {
         int x, y, largura, altura;
         if (sscanf(comando, "create %d,%d+%d,%d", &x, &y, &largura, &altura) == 4) {
             Retangulo novo = criar_retangulo(x, y, largura, altura);
-            if (!adicionar_retangulo(plano, novo)) {
-                printf("Erro: Não foi possivel criar o retangulo.\n");
+            if (adicionar_retangulo(plano, novo)) {
+                exibir_posicao_retangulo(&novo);
+            } else {
+                printf("Erro: Não foi possível criar o retângulo.\n");
             }
         } else {
-            printf("Comando invalido.\n");
+            printf("Comando inválido.\n");
         }
     } else if (strncmp(comando, "moveleft", 8) == 0) {
         int x, y, p;
@@ -29,11 +35,12 @@ void processar_comando(Plano* plano, const char* comando) {
             int indice = encontrar_retangulo_por_ponto(plano, x, y);
             if (indice != -1) {
                 mover_retangulo_no_plano(plano, indice, p, 0);
+                exibir_posicao_retangulo(&plano->retangulos[indice]);
             } else {
-                printf("Erro: Retangulo nao encontrado.\n");
+                printf("Erro: Retângulo não encontrado.\n");
             }
         } else {
-            printf("Comando invalido.\n");
+            printf("Comando inválido.\n");
         }
     } else if (strncmp(comando, "moveright", 9) == 0) {
         int x, y, p;
@@ -41,11 +48,12 @@ void processar_comando(Plano* plano, const char* comando) {
             int indice = encontrar_retangulo_por_ponto(plano, x, y);
             if (indice != -1) {
                 mover_retangulo_no_plano(plano, indice, p, 1);
+                exibir_posicao_retangulo(&plano->retangulos[indice]);
             } else {
-                printf("Erro: Retangulo nao encontrado.\n");
+                printf("Erro: Retângulo não encontrado.\n");
             }
         } else {
-            printf("Comando invalido.\n");
+            printf("Comando inválido.\n");
         }
     } else if (strncmp(comando, "exit", 4) == 0) {
         printf("Saindo...\n");
@@ -54,4 +62,5 @@ void processar_comando(Plano* plano, const char* comando) {
     }
 
     desenhar_plano(plano);
+    
 }
--- menu.h
+++ menu.h
@@ -5,6 +5,7 @@
 
 void exibir_menu();
 void processar_comando(Plano* plano, const char* comando);
+void exibir_posicao_retangulo(const Retangulo* ret);
 
 #endif // MENU_H
 
